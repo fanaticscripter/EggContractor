a0652dfe64b9d3fede174acd58729018435cc9f6
fanaticscripter <fanaticscripter@gmail.com>
2021-02-17 23:37:20 +0800

    port/wasm/artifact-explorer: link to consumption sheet

--- port/wasm/artifact-explorer/src/views/Artifact.vue
+++ port/wasm/artifact-explorer/src/views/Artifact.vue
@@ -217,6 +217,53 @@
       </div>
     </template>
 
+    <hr />
+    <div class="px-4 py-4 sm:px-6 space-y-1">
+      <div>
+        <span class="text-sm font-medium text-gray-500 mr-1">
+          What do I get from consuming this item?
+        </span>
+        <a
+          :href="`https://wasmegg.netlify.app/consumption-sheet/#${artifact.id}`"
+          target="_blank"
+          class="inline-flex items-center border-dashed border-b border-gray-700 text-sm whitespace-nowrap leading-tight space-x-0.5"
+        >
+          <span>Consumption sheet</span>
+          <svg viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4" data-html2canvas-ignore>
+            <path
+              d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+            />
+            <path
+              d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+            />
+          </svg>
+        </a>
+      </div>
+      <div
+        v-if="artifact.type === 'Stone' || artifact.type === 'Stone ingredient'"
+      >
+        <span class="text-sm font-medium text-gray-500 mr-1">
+          Consuming which items yields this
+          {{ artifact.type === "Stone" ? "stone" : "stone fragment" }}?
+        </span>
+        <a
+          :href="`https://wasmegg.netlify.app/consumption-sheet/#${artifact.id}-sources`"
+          target="_blank"
+          class="inline-flex items-center border-dashed border-b border-gray-700 text-sm whitespace-nowrap leading-tight space-x-0.5"
+        >
+          <span>Consumption sheet</span>
+          <svg viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4" data-html2canvas-ignore>
+            <path
+              d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+            />
+            <path
+              d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+
     <div class="px-4 pb-4 sm:px-6 text-xs text-gray-500">&dagger; Not available from missions.</div>
   </div>
 </template>
