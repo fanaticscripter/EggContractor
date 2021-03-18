9e5c321eef653387472cf4b9007ddf1e991dacaa
fanaticscripter <fanaticscripter@gmail.com>
2021-03-18 23:29:20 +0800

    port/wasm/artifact-explorer: link to /loot-analysis/

--- port/wasm/artifact-explorer/src/components/LootDataCredit.vue
+++ port/wasm/artifact-explorer/src/components/LootDataCredit.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="text-xs text-gray-500">
-    Drop rate data based on user contributions to
-    <a
-      href="https://ei.mikit.app/conribute_data"
-      target="_blank"
-      class="text-gray-500 hover:text-gray-700 underline"
-      >ei.mikit.app</a
-    >. Visit to contribute yours!
+    <p>
+      Drop rate data based on user contributions to
+      <a
+        href="https://ei.mikit.app/conribute_data"
+        target="_blank"
+        class="text-gray-500 hover:text-gray-700 underline"
+        >ei.mikit.app</a
+      >. Visit to contribute yours!
+    </p>
+    <p>
+      Rigorous statistical analysis of drop rate data can be found at
+      <a
+        href="https://wasmegg.netlify.app/loot-analysis/"
+        target="_blank"
+        class="text-gray-500 hover:text-gray-700 underline"
+        >/loot-analysis/</a
+      >.
+    </p>
   </div>
 </template>
 
