fa04aa2dc1b90e17fc0402f84ec44bb49f015590
fanaticscripter <fanaticscripter@gmail.com>
2021-04-30 22:24:09 +0800

    port/wasm/{artifact-sandbox,proto-explorer}: remove h-stretch...
    and min-h-stretch classes.
    
    They are used on html and body only, and when combined with h-screen and
    min-h-screen cause a cssConflict warning from Tailwind intellisense. Just
    write the CSS directly.

--- port/wasm/artifact-sandbox/tailwind.config.js
+++ port/wasm/artifact-sandbox/tailwind.config.js
@@ -42,16 +42,6 @@ module.exports = {
           90: "hsl(0, 0%, 90%)",
         },
       },
-      // h-stretch and min-h-stretch fix the 100vh != actual viewport height
-      // issue in iOS Safari. autoprefixer should expand stretch to
-      // -webkit-fill-available for Chrome/Safari.
-      // https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/
-      height: {
-        stretch: "stretch",
-      },
-      minHeight: {
-        stretch: "stretch",
-      },
     },
   },
   variants: {
--- port/wasm/proto-explorer/tailwind.config.js
+++ port/wasm/proto-explorer/tailwind.config.js
@@ -3,20 +3,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   purge: ["src/*.html", "src/**/*.vue"],
   darkMode: false,
-  theme: {
-    extend: {
-      // h-stretch and min-h-stretch fix the 100vh != actual viewport height
-      // issue in iOS Safari. autoprefixer should expand stretch to
-      // -webkit-fill-available for Chrome/Safari.
-      // https://allthingssmitty.com/2020/05/11/css-fix-for-100vh-in-mobile-webkit/
-      height: {
-        stretch: "stretch",
-      },
-      minHeight: {
-        stretch: "stretch",
-      },
-    },
-  },
   variants: {
     extend: {
       opacity: ["disabled"],
