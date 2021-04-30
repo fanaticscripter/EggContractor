9b93bbcd46a9553191c0eac1ee0ceaf3d83f854d
fanaticscripter <fanaticscripter@gmail.com>
2021-04-30 22:18:54 +0800

    port/wasm/artifact-sandbox: drop Google Analytics and associated notice

--- port/wasm/artifact-sandbox/src/App.vue
+++ port/wasm/artifact-sandbox/src/App.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="max-w-3xl w-full mx-auto px-4 mb-4 text-sm leading-tight space-y-2">
-    <div class="text-red-500">
-      You are using an alpha build of this tool. There is no compatibility guarantee for shared
-      links, and many possible improvements have not yet been implemented. Future development hinges
-      on community response, in terms of traffic and perceived mind share. As such, basic
-      information about your visit (e.g. your browser brand and screen size) is collected through
-      Google Analytics in order to guide development. Feedback is welcome on
-      <a href="https://discord.gg/egginc" target="_blank" class="underline hover:text-dark-70"
-        >Discord</a
-      >
-      and
-      <a
-        href="https://github.com/fanaticscripter/EggContractor/discussions"
-        target="_blank"
-        class="underline hover:text-dark-70"
-        >GitHub</a
-      >.
-    </div>
     <div v-if="activeNews.length > 0" class="text-green-500">
       <p class="uppercase">What's New</p>
       <ul v-for="(news, index) in activeNews" :key="index">
