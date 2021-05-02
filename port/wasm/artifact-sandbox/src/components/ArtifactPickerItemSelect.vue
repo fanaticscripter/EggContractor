180b8d770109fac6c895e39d103eb8fe9bb5144c
fanaticscripter <fanaticscripter@gmail.com>
2021-05-02 17:08:45 +0800

    artifact-sandbox: allow clearing selected artifacts and stones

--- port/wasm/artifact-sandbox/src/components/ArtifactPickerItemSelect.vue
+++ port/wasm/artifact-sandbox/src/components/ArtifactPickerItemSelect.vue
@@ -28,7 +28,7 @@
         <input
           ref="selectButton"
           type="text"
-          class="focus:ring-blue-500 focus:border-blue-500 block w-full pl-11 pr-10 pt-2.5 pb-2 sm:text-sm bg-dark-20 rounded-md placeholder-white"
+          class="focus:ring-blue-500 focus:border-blue-500 block w-full pl-11 pr-12 pt-2.5 pb-2 sm:text-sm bg-dark-20 rounded-md placeholder-white"
           :class="
             selected && searchFilter === selected.display && selected.afx_rarity > 0
               ? selected.rarity
@@ -56,6 +56,26 @@
             <path
               fill-rule="evenodd"
               d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
+        <div
+          v-if="selected"
+          class="absolute inset-y-0 right-7 flex items-center cursor-pointer"
+          @click="clear"
+        >
+          <!-- Heroicon name: solid/x -->
+          <svg
+            class="h-4 w-4 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
               clip-rule="evenodd"
             />
           </svg>
@@ -167,7 +187,6 @@ export default defineComponent({
   },
   emits: {
     "update:modelValue": itemId => true,
-    delete: () => true,
   },
   setup(props, { emit }) {
     // Note that we do NOT react to type change.
@@ -281,8 +300,9 @@ export default defineComponent({
       }
     };
 
-    const deleteEntry = () => {
-      emit("delete");
+    const clear = () => {
+      emit("update:modelValue", "");
+      searchFilter.value = "";
     };
 
     return {
@@ -299,7 +319,7 @@ export default defineComponent({
       activateItem,
       deactivateItem,
       handleKeydown,
-      deleteEntry,
+      clear,
       iconURL,
     };
   },
