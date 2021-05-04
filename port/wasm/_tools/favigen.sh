f8e41c24afe9ef6c27248f5d8291c894865ca9ad
fanaticscripter <fanaticscripter@gmail.com>
2021-05-04 19:57:05 +0800

    wasmegg: favigen.sh: add support for bordered variant

--- port/wasm/_tools/favigen.sh
+++ port/wasm/_tools/favigen.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env zsh
 setopt errexit nounset pipefail
+bordered=0
+if [[ $1 == -b || $1 == --border ]]; then
+    bordered=1
+    shift
+fi
 color=$1
 output_dir=$2
 [[ -n $color && -n $output_dir ]] || {
     cat >&2 <<EOF
-$0 <color> <output_directory>
+$0 [-b|--border] <color> <output_directory>
 
 Example:
 
   $0 '#ff0000' favicon.ico
 
 to generate a red version of the favicon.
+
+-b, --border adds a gray border to the favicon (used for primary colors with too little contract to a light/dark background.)
 EOF
     exit 1
 }
-here=$0:A:h
-orig_svg=$here/../../../static/favicon.svg
-orig_svg=$orig_svg:A
 tmpdir="$(mktemp -d)"
 echo "working directory: $tmpdir"
 output_svg=$output_dir/favicon.svg
-sed "s/fill=\"#6b7280\"/fill=\"$color\"/" <$orig_svg >$output_svg
+if (( bordered )); then
+    cat >$output_svg <<EOF
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-96 -32 576 576"><path fill="$color" d="M192 0C86 0 0 214 0 320s86 192 192 192 192-86 192-192S298 0 192 0z" /><path fill="#6b7280" d="M192 0C86 0 0 214 0 320s86 192 192 192 192-86 192-192S298 0 192 0zm0 480c-88.22 0-160-71.78-160-160 0-100.72 82.24-288 160-288s160 187.28 160 288c0 88.22-71.78 160-160 160z" /></svg>
+EOF
+else
+    cat >$output_svg <<EOF
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="-96 -32 576 576"><path fill="$color" d="M192 0C86 0 0 214 0 320s86 192 192 192 192-86 192-192S298 0 192 0z" /></svg>
+EOF
+fi
 for size in 16 32 48; do
     convert -background transparent -density 300 $output_svg -resize ${size}x${size} $tmpdir/favicon-$size.png
 done
