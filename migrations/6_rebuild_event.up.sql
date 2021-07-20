ff5f706cf0b0d7ff92a507b8f561a844ed5610b4
fanaticscripter <fanaticscripter@gmail.com>
2021-07-20 21:44:23 +0800

    migrations/6_rebuild_event.up.sql: fix inconsistent indentation

--- migrations/6_rebuild_event.up.sql
+++ migrations/6_rebuild_event.up.sql
@@ -4,13 +4,13 @@ PRAGMA foreign_keys = off;
 
 CREATE TABLE IF NOT EXISTS event_new (
     rowid INTEGER PRIMARY KEY,
-	id TEXT NOT NULL,
-	event_type TEXT NOT NULL,
-	multiplier REAL NOT NULL,
-	message TEXT NOT NULL,
-	first_seen_timestamp REAL NOT NULL,
-	last_seen_timestamp REAL NOT NULL,
-	expiry_timestamp REAL NOT NULL
+    id TEXT NOT NULL,
+    event_type TEXT NOT NULL,
+    multiplier REAL NOT NULL,
+    message TEXT NOT NULL,
+    first_seen_timestamp REAL NOT NULL,
+    last_seen_timestamp REAL NOT NULL,
+    expiry_timestamp REAL NOT NULL
 );
 
 INSERT INTO event_new(
